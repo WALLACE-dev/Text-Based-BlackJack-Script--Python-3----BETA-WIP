7d0a9451815077eae662a9e46dcd82bb9b6c4ae9
WALLACE-dev <54773341+WALLACE-dev@users.noreply.github.com>
2019-10-10 22:42:16 -0400

    Update BlackJack_Beta_V0.1.py
    Removed the "Welcome" Tag on new rounds.

--- Beta/BlackJack_Beta_V0.1.py
+++ Beta/BlackJack_Beta_V0.1.py
@@ -46,7 +46,6 @@ def game(bank, winnings):
     cards = [1,2,3,4,5,6,7,8,9,10,10,10]
     dealerCards = []
     playerCards = []
-    print("Welcome to WALLACES BlackJack Beta Version 0.5")
     time.sleep(1.5)
     print("\nBank: {}".format(bank))
     print("Winnings: {}".format(winnings))
